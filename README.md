792ee2ec9162a319e8b7c55225fd3060018bdeec
Chen Zhen2 <chenzhen2@aactechnologies.com>
2020-05-18 19:22:54 +0800

    回顾笔记，并做适当调整

--- README.md
+++ README.md
@@ -23,60 +23,19 @@ Redis 的优点：
 + 原子性：Redis 的所有操作都是原子性的，同时 Redis 还支持对几个操作合并后的原子性执行
 + 丰富的特性：支持 publish/subscribe、通知、key过期等特性
 
-### 基本数据类型及相关操作（命令）
-
-[命令查询](https://redis.io/commands)
-
-#### 1、字符串-string
-+ 单个设置（keyname 不存在时为创建，存在时为替换）：set keyname val
-+ 单个设置强制为创建（keyname 不存在时才成功）：set keyname val nx
-+ 单个设置强制为替换（keyname 存在时才成功）：set keyname val xx
-+ 单个获取：get keyname
-+ 加减1（只有当 keyname 对应的 value 是 number 类型的字符串）：incr/decr keyname
-+ 加减（只有当 keyname 对应的 value 是 number 类型的字符串）：incrby/decrby keyname number
-+ 设置多个：mset keyname1 val [keyname2 val]
-+ 获取多个：mget keyname1 [keyname2]
-
-#### 2、列表-list
-
-+ 头部插入：lpush keyname val1 [val2] 
-+ 尾部插入：rpush keyname val1 [val2] 
-+ 获取指定范围（0 表示 list 开头第一个，-1 表示 list 的倒数第一个）：lrange keyname 0 -1
-+ 头部删除：lpop keyname
-+ 尾部删除：rpop keyname
-
-#### 3、无序集合-set
-
-+ 添加：sadd keyname val1 [val2]
-+ 查看集合：smembers keyname
-+ 查看集合元素是否存在：sismember keyname val
-
-#### 4、散列-hash
-
-+ 设置一个多字段的 hash 表：hmset keyname field1 val1 [field2 val2]
-+ 获取指定字段：hget keyname filedname
-+ 获取所有字段：hgetall keyname
-+ 获取多个字段：hmget keyname filed1 [filed2]
-+ 数字加操作：hincrby keyname filedname number
-
-#### 5、有序集合-zset（orderedset）
-
-+ 添加：zadd keyname score val
-+ 查看指定范围正序的集合（withscores 参数返回 scores）：zrange keyname 0 -1
-+ 查看指定范围反序的集合（withscores 参数返回 scores）：zrevrange keyname 0 -1
-
-
 
 ### Redis 系统命令
 
+[命令查询](https://redis.io/commands)
+
 #### 1、适合全体类型的常用命令
 
-+ 判断 key 是否存在（存在返回1，否则返回0）：
-+ 删除 key（成功返回1，否则返回0）：
++ 判断 key 是否存在（存在返回1，否则返回0）：exists keyname
++ 删除 key（成功返回1，否则返回0）：del keyname
 + 返回 key 对应 value 的数据类型（none/string/list/set/zset（有序集合）/hash）：type keyname
 + 返回匹配的 key 列表：keys key-pattern
 + 随机获取一个已存在的 key：randomkey
-+ 清楚界面：clear
++ 清空界面：clear
 + 更改 key 的名字（如果新键存在将被覆盖）：rename oldname newname
 + 更改 key 的名字（如果新键存在更新失败）：renamenx oldname newname
 + 返回当前数据库中 key 的总数：dbsize
--- Redis-Java/src/main/java/org/daistudy/redisjava/redisinaction/Chapter001.java
+++ Redis-Java/src/main/java/org/daistudy/redisjava/redisinaction/Chapter001.java
@@ -6,32 +6,35 @@ import redis.clients.jedis.ZParams;
 import java.util.*;
 
 /**
- * 需求：使用 Redis 构建一个简单的文章投票网站的后端
- * 网站根据文章的发布时间和文章获得的投票数量计算出一个评分，按照评分决定如何排序和展示文章
+ * 需求：使用 Redis 构建一个简单的文章投票网站后端
+ *      网站根据文章的发布时间和文章获得的投票数量计算出一个评分，并可以按照评分决定如何排序和展示文章
  *
- * 评分可以随着时间流逝而不断减少（计算方法）：文章得到的支持票数乘以一个常量 + 文章的发布时间 == 文章的评分
- * 从Unix时间（UTC时区1970年1月1日）到现在为止经过的秒数来计算文章的评分
+ * 评分：可以随着时间流逝而不断减少（暂时先不做）
+ *      文章的评分 = 文章得到的支持票数乘以一个常量【 + 文章的发布时间】评分计算方法有问题
+ *      从Unix时间（UTC时区1970年1月1日）到现在为止经过的秒数来计算文章的评分
  *
- * 文章信息使用 Redis 中的 Hash 结构存储 -- hash article:articleId 默认投票数量为0
- * 根据发布时间排序文章 -- zset time:
- * 根据评分排序文章 -- zset score:
- * 根据发布时间排序群组文章 -- zset time:groupId == time: 和 group:groupId 的 zinterstore
- * 根据评分排序群组文章 -- zset score:groupId == score: 和 group:groupId 的 zinterstore
- * 每篇文章的已投票用户名单（不能重复投票） -- set voted:articleId
- * 用户 -- user:userId
- * 用户集合 -- set user:list
- * 文章集合 -- set article:list
- * 文章ID的计数器 -- string article:
- * 用户ID的计数器 -- string user:
- * 群组ID的计数器 -- string group:
- * 同属一个群组中的文章ID记录集合 -- set group:groupId
- * 规定当一篇文章发布期满一周之后，用户将不能再对其进行投票，文章的评分将被固定下来，而文章的已投票用户名单的集合也会被删除
+ * 信息及其存储结构：
+ *      单个文章信息 -- hash -- article:articleId 其中投票数量默认为1
+ *      根据发布时间排序的文章列表 -- zset -- time:
+ *      根据评分排序的文章列表 -- zset -- score:
+ *      根据发布时间排序的群组的文章列表 -- zset -- time:groupId （该集合是 time: 和 group:groupId 的 zinterstore）
+ *      根据评分排序的群组的文章列表 -- zset -- score:groupId （该集合是 score: 和 group:groupId 的 zinterstore）
+ *      每篇文章的已投票用户名单（不能重复投票） -- set -- voted:articleId（需要设置过期时间，单纯给一个没有元素的键设置过期时间，好像是没有效果）
+ *      用户集合 -- set -- user:list（集合元素的值为 user:userId）
+ *      文章集合 -- set -- article:list（集合元素的值为 article:articleId）
+ *      群组集合 -- set -- group:list（集合元素的值为 group:groupId）
+ *      文章ID的计数器 -- string -- article:
+ *      用户ID的计数器 -- string -- user:
+ *      群组ID的计数器 -- string -- group:
+ *      同属一个群组中的文章ID记录集合 -- set -- group:groupId
  *
- * 群组存放文章
+ * 规定：
+ *      当一篇文章发布期满一周之后，用户将不能再对其进行投票，文章的评分将被固定下来，而文章的已投票用户名单的集合也会被删除
+ *
  */
 public class Chapter001 {
     private static final int ONE_WEEK_IN_SECONDS = 7 * 86400;
-    private static final int VOTE_SCORE = 432;
+    private static final int VOTE_SCORE = 10;
     private static final int ARTICLES_PER_PAGE = 25;
 
     public static void main(String[] args) {
@@ -50,29 +53,17 @@ public class Chapter001 {
         int i = 0;
         while(i < count){
             userRandom = conn.srandmember("user:list");
-
-            String articleId = postArticle(
-                    conn, userRandom, "A title", "http://www.google.com");
-            System.out.println(userRandom + "发布了一篇文章，ID号为: " + articleId);
-            System.out.println("文章内容如下:");
-            Map<String,String> articleData = conn.hgetAll("article:" + articleId);
-            for (Map.Entry<String,String> entry : articleData.entrySet()){
-                System.out.println("  " + entry.getKey() + ": " + entry.getValue());
-            }
-
-            System.out.println();
+            postArticle(conn, userRandom, "A title", "http://www.google.com");
             i++;
         }
 
-
-        count = random.nextInt(1000);
         i = 0;
         while(i < count){
             userRandom = conn.srandmember("user:list");
             articleRandom = conn.srandmember("article:list");
             articleVote(conn, userRandom, articleRandom);
             String votes = conn.hget(articleRandom, "votes");
-            System.out.println("有人给文章" + articleRandom + "投票了，现在票数是： " + votes);
+            System.out.println("用户"+ userRandom +"给文章" + articleRandom + "投票了，现在票数是： " + votes);
             assert Integer.parseInt(votes) > 1;
             i++;
         }
@@ -80,7 +71,7 @@ public class Chapter001 {
 
         System.out.println("目前评分最高的文章列表:");
         List<Map<String,String>> articles = getArticles(conn, 1);
-        printArticles(articles);
+        printArticles(conn, articles);
         assert articles.size() >= 1;
 
 
@@ -91,11 +82,12 @@ public class Chapter001 {
 //        assert articles.size() >= 1;
     }
 
-    private void printArticles(List<Map<String, String>> articles) {
+    private void printArticles(Jedis conn, List<Map<String, String>> articles) {
         for (Map<String,String> article : articles){
             System.out.println("  id: " + article.get("id"));
             for (Map.Entry<String,String> entry : article.entrySet()){
                 if (entry.getKey().equals("id")){
+                    System.out.println("    评分: " + conn.zscore("score:", "article:" + article.get("id")));
                     continue;
                 }
                 System.out.println("    " + entry.getKey() + ": " + entry.getValue());
@@ -103,6 +95,9 @@ public class Chapter001 {
         }
     }
 
+    /**
+     * Jedis 的初始化
+     */
     public Jedis init(){
         Jedis conn = new Jedis("localhost");
         conn.select(15);
@@ -112,22 +107,35 @@ public class Chapter001 {
         return conn;
     }
 
-    public void generateUsers(Jedis conn){
+    /**
+     * 创建用户集合
+     * @param conn Jedis
+     */
+    private void generateUsers(Jedis conn){
         genereteSet(conn, "user:");
     }
 
-    public void generateGroups(Jedis conn){
+    /**
+     * 创建群组集合
+     * @param conn Jedis
+     */
+    private void generateGroups(Jedis conn){
         genereteSet(conn, "group:");
     }
 
+    /**
+     * 创建集合
+     * @param conn Jedis
+     * @param key 集合的键
+     */
     private void genereteSet(Jedis conn, String key){
         if(!conn.exists(key)){
-            conn.set(key, "100000");
+            conn.set(key, "10000");
         }
-        // 随机递增（1-101）生成1000个
+        // 随机递增（1-101）生成100个值
         Random random = new Random();
         int i = 0;
-        while(i < 1000){
+        while(i < 100){
             int incr = random.nextInt(100) + 1;
             conn.incrBy(key, incr);
             conn.sadd(key + "list", key + conn.get(key));
@@ -136,7 +144,7 @@ public class Chapter001 {
     }
 
     /**
-     * user 用户给 article 文章投票
+     * 文章投票
      * @param conn Redis
      * @param user 用户
      * @param article 文章
@@ -147,8 +155,8 @@ public class Chapter001 {
         if (conn.zscore("time:", article) < cutoff) {
             return;
         }
-        // 2. 未满一周才进行投票
 
+        // 2. 未满一周才进行投票
         String articleId = article.substring(article.indexOf(":") + 1);
         // 2.1 用户投票成功（不能重复投票）--文章已投票用户名单增加用户
         if (conn.sadd("voted:" + articleId, user) == 1) {
@@ -161,20 +169,20 @@ public class Chapter001 {
 
     /**
      * 发布文章
-     * @param conn Redis
+     * @param conn Jedis
      * @param user 发布人
      * @param title 发布文章的标题
      * @param link 发布文章的链接
      */
-    public String postArticle(Jedis conn, String user, String title, String link){
+    private String postArticle(Jedis conn, String user, String title, String link){
         // 1.1 生成文章ID
-        String articleId;
+
         if (conn.exists("article:")) {
-            articleId = conn.incrBy("article:", new Random().nextInt(1000) + 1).toString();
+            conn.incrBy("article:", new Random().nextInt(100) + 1).toString();
         }else{
-            articleId = "100000";
-            conn.set("article:", articleId);
+            conn.set("article:", "10000");
         }
+        String articleId = conn.get("article:");
         String article = "article:" + articleId;
 
         // 1.2 将文章添加到文章集合中
@@ -182,8 +190,8 @@ public class Chapter001 {
 
         // 2. 生成文章已投票用户名单
         String voted = "voted:" + articleId;
-//        conn.sadd(voted, user); // 是否需要默认发布人给自己发布文章投一票
-        conn.expire(voted, ONE_WEEK_IN_SECONDS); // 集合的键不存在，是否可以设置过期时间？？？？
+        conn.sadd(voted, user);
+        conn.expire(voted, ONE_WEEK_IN_SECONDS);
 
         // 3. 设置文章信息
         long now = System.currentTimeMillis() / 1000;
@@ -193,22 +201,35 @@ public class Chapter001 {
         articleData.put("link", link);
         articleData.put("user", user);
         articleData.put("now", String.valueOf(now));
-        articleData.put("votes", "0"); // 是否需要默认发布人给自己发布文章投一票
+        articleData.put("votes", "1");
         conn.hmset(article, articleData);
 
-        // 4. 设置文章评分
-        conn.zadd("score:", now + VOTE_SCORE, article);
+        // TODO 4. 设置文章评分（评分机制有问题）
+        conn.zadd("score:", VOTE_SCORE, article);
 
-        // 5. 设置文章时间
+        // 5. 按时间排序的文章
         conn.zadd("time:", now, article);
 
         return articleId;
     }
 
+    /**
+     * 获取文章
+     * @param conn Jedis
+     * @param page 第几页
+     * @return 默认从评分集合中获取
+     */
     public List<Map<String, String>> getArticles(Jedis conn, int page){
         return getArticles(conn, page, "score:");
     }
 
+    /**
+     * 获取文章
+     * @param conn Jedis
+     * @param page 第几页
+     * @param order 排序方式 == 从哪个集合中获取文章
+     * @return 文章列表
+     */
     public List<Map<String, String>> getArticles(Jedis conn, int page, String order){
         // 1. 获取第 page 页的文章ID
         int start = (page - 1) * ARTICLES_PER_PAGE;
@@ -219,7 +240,6 @@ public class Chapter001 {
         List<Map<String, String>> articles = new ArrayList<>();
         for (String articleId : articleIds) {
             Map<String, String> articleData = conn.hgetAll(articleId);
-            articleData.put("id", articleId);
             articles.add(articleData);
         }
         return articles;
@@ -227,12 +247,11 @@ public class Chapter001 {
 
     /**
      * 将文章添加到群组中
-     * @param conn Redis
-     * @param articleId 文章 -- 应该传入 articleId 还是 article:articleId
+     * @param conn Jedis
+     * @param article 文章
      * @param toAdd 群组
      */
-    public void addArticleToGroups(Jedis conn, String articleId, String[] toAdd){
-        String article = "article:" + articleId;
+    public void addArticleToGroups(Jedis conn, String article, String[] toAdd){
         for (String group : toAdd) {
             conn.sadd("group:" + group, article);
         }
